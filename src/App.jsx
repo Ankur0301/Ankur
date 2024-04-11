cb0ca6e13c8ac6a93dcb7179cc82f0995021022b
Ankur Agrawal <86463188+Ankur0301@users.noreply.github.com>
2024-04-11 15:57:36 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Ankur Agrawal",
+  title: "MERN Stack Developer",
+  email: "ankur@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "Ankur Agrawal",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
